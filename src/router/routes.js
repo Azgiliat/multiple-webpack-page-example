d043d7a64c5b0a8f468f7d056cb05cc1a2e8ecdf
azgiliat <toxa2088@gmail.com>
2023-01-20 20:54:05 +0300

    example with routing in browser

---
+++ src/router/routes.js
@@ -0,0 +1,18 @@
+export const routes = [
+    {
+        path: '/',
+        page: () => import('@/pages/main')
+    },
+    {
+        path: 'main',
+        page: () => import('@/pages/main')
+    },
+    {
+        path: 'news',
+        page: () => import('@/pages/news')
+    },
+    {
+        path: 'profile',
+        page: () => import('@/pages/profile')
+    }
+]
